eb4f1039658c442a260fde15dbb574072dfa3178
Andrés <alindarte@unal.edu.co>
2026-05-27 18:51:17 -0500

    Fixed useful.py

--- useful.py
+++ useful.py
@@ -9,54 +9,42 @@ class UsefulFunctions:
     
     """
 
-    def __init__(self,
-                vector=None,        # to verify_coords
-                h=None, m=None, s=None,      # to verify_hms, hms_to_degrees
-                n=None,            # to verify_1or2, verify_dms
-                d=None, #m=None, s=None,     # to verify_dms, dms_to_degrees
-                rad=None,          # to radians_to_dms, radians_to_hms
-                ):
-        
-        self.vector = vector
-        self.h = h
-        self.m = m
-        self.s = s
-        self.n = n
-        self.d = d
-        self.rad = rad  
-
-            # Verifications
-    def verify_1or2(self):
-        if self.n not in (1, 2):
+    @staticmethod
+    def verify_1or2(n):
+        if n not in (1, 2):
             raise ValueError("Error: Number must be 1 or 2")
 
-    def verify_coords(self):
-        if self.vector.size != 3:
+    
+    @staticmethod
+    def verify_coords(vector):
+        if vector.size != 3:
             raise ValueError("Error: Coordinates must be a size 3 vector.")
 
-    def verify_dms(self):  # n is the maximum value, i.e., 90 or 360
+    @staticmethod
+    def verify_dms(n, d, m, s):  # n is the maximum value, i.e., 90 or 360
 
-        if self.n == 90:
-            if abs(self.d) > 90:
+        if n == 90:
+            if abs(d) > 90:
                 raise ValueError("Degrees must be between -90 and 90.")
-        elif self.n == 360:
-            if not (0 <= self.d < 360):
+        elif n == 360:
+            if not (0 <= d < 360):
                 raise ValueError("Degrees must be between 0 and 360.")
-        if not (0 <= self.m < 60):
+        if not (0 <= m < 60):
             raise ValueError("Arcminutes must be between 0 and 60.")
-        if not (0 <= self.s < 60):
+        if not (0 <= s < 60):
             raise ValueError("Arcseconds must be between 0 and 60.")
 
         return True
 
 
-    def verify_hms(self):
+    @staticmethod
+    def verify_hms(h, m, s):
 
-        if not (0 <= self.h < 24):
+        if not (0 <= h < 24):
             raise ValueError("Hours must be between 0 and 24.")
-        if not (0 <= self.m < 60):
+        if not (0 <= m < 60):
             raise ValueError("Minutes must be between 0 and 60.")
-        if not (0 <= self.s < 60):
+        if not (0 <= s < 60):
             raise ValueError("Seconds must be between 0 and 60.")
 
         return True
@@ -65,24 +53,27 @@ class UsefulFunctions:
         # Conversions
 
             # From Degrees-Minutes-Seconds (DMS) to Degrees
-    def dms_to_degrees(self):
+    @staticmethod
+    def dms_to_degrees(d, m, s):
 
-        sign = -1 if self.d < 0 else 1
-        d = abs(self.d)
+        sign = -1 if d < 0 else 1
+        d = abs(d)
 
-        return sign * (d + self.m/60 + self.s/3600)
+        return sign * (d + m/60 + s/3600)
 
 
             # From Hours-Minutes-Seconds (HMS) to Degrees
-    def hms_to_degrees(self):
+    @staticmethod
+    def hms_to_degrees(h, m, s):
 
-        return 15 * (self.h + self.m/60 + self.s/3600)
+        return 15 * (h + m/60 + s/3600)
 
 
             # From Radians to DMS
-    def radians_to_dms(self):
+    @staticmethod
+    def radians_to_dms(rad):
 
-        deg = np.degrees(self.rad)
+        deg = np.degrees(rad)
         sign = -1 if deg < 0 else 1
         deg = abs(deg)
 
@@ -94,9 +85,10 @@ class UsefulFunctions:
 
 
             # From Radians to HMS
-    def radians_to_hms(self):
+    @staticmethod
+    def radians_to_hms(rad):
 
-        rad = self.rad % (2*np.pi)
+        rad = rad % (2*np.pi)
 
         total_hours = np.degrees(rad) / 15
 
